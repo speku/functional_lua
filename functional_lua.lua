680ef1b94a30d75f171cbdc524a9f268d8e46612
Maximilian Roeck <mroeck@stud.hs-heilbronn.de>
2017-01-06 22:07:51 +0100

    added lazyness

--- functional_lua.lua
+++ functional_lua.lua
@@ -1,9 +1,10 @@
 -- functions for programming in a functional style
 
 function compose(...)
+	local args = {...}
 	return function(x)
-	  for _,f in ipairs{...} do
-		x = f(x)
+	  for _,f in ipairs(args) do
+			x = f(x)
 	  end
 	  return x
 	end
@@ -23,72 +24,72 @@ function uncurry(f)
 	end
 end
 
-function map(f,xs)
-	local _xs = {}
-	for k,v in pairs(xs) do
-		_xs[k] = f(v)
-	end
-	return _xs
-end
+-- function map(f,xs)
+-- 	local _xs = {}
+-- 	for k,v in pairs(xs) do
+-- 		_xs[k] = f(v)
+-- 	end
+-- 	return _xs
+-- end
+--
+-- function mapi(f,xs)
+-- 	local _xs = {}
+-- 	for _,v in ipairs(xs) do
+-- 		table.insert(_xs,f(v))
+-- 	end
+-- 	return _xs
+-- end
 
-function mapi(f,xs)
-	local _xs = {}
-	for _,v in ipairs(xs) do
-		table.insert(_xs,f(v))
-	end 
-	return _xs
-end
+-- function filter(p,xs)
+-- 	local _xs = {}
+-- 	for k,v in pairs(xs) do
+-- 		if p(v) then
+-- 			_xs[k] = v
+-- 		end
+-- 	end
+-- 	return _xs
+-- end
+--
+-- function filteri(p,xs)
+-- 	local _xs = {}
+-- 	for _,v in ipairs(xs) do
+-- 		if p(v) then
+-- 			table.insert(_xs,v)
+-- 		end
+-- 	end
+-- 	return _xs
+-- end
 
-function filter(p,xs)
-	local _xs = {}
-	for k,v in pairs(xs) do
-		if p(v) then 
-			_xs[k] = v
-		end
-	end
-	return _xs 
-end
+-- function fold(f,a,xs)
+-- 	local _a = a
+-- 	for _,v in pairs(xs) do
+-- 		_a = f(_a,v)
+-- 	end
+-- 	return _a
+-- end
+--
+-- function foldi(f,a,xs)
+-- 	local _a = a
+-- 	for _,v in ipairs(xs) do
+-- 		_a = f(_a,v)
+-- 	end
+-- 	return _a
+-- end
+--
+--
+-- function fold_(f,xs)
+-- 	local _a
+-- 	for k,v in pairs(xs) do
+-- 		if _a == nil then
+-- 			_a = v
+-- 		else
+-- 			_a = f(_a,v)
+-- 		end
+-- 	end
+-- 	return _a
+-- end
 
-function filteri(p,xs)
-	local _xs = {}
-	for _,v in ipairs(xs) do
-		if p(v) then 
-			table.insert(_xs,v)
-		end
-	end
-	return _xs 
-end
-
-function fold(f,a,xs)
-	local _a = a 
-	for _,v in pairs(xs) do
-		_a = f(_a,v)
-	end
-	return _a
-end
-
-function foldi(f,a,xs)
-	local _a = a 
-	for _,v in ipairs(xs) do
-		_a = f(_a,v)
-	end
-	return _a
-end
-
-
-function fold_(f,xs)
-	local _a 
-	for k,v in pairs(xs) do
-		if _a == nil then
-			_a = v
-		else
-			_a = f(_a,v)
-		end
-	end 
-	return _a
-end
-
-function _papply(f,x)
+local function _papply(f,x)
 	return function(...)
 		return f(x,...)
 	end
@@ -103,8 +104,47 @@ end
 
 function range(s,f,st)
 	local r = {}
-	for i=s,f,st or 1 do 
+	for i=s,f,st or 1 do
 		table.insert(r,i)
-	end 
+	end
 	return r
+end
+
+
+-- local function _foldr(f,a,...)
+-- 	if select("#",...) == 0 then
+-- 		return a
+-- 	else
+-- 		return f(select(1,...),function() return _foldr(f,a,select(2,...)) end) -- _foldr(f,f(a,select(1,...)),select(2,...))
+-- 	end
+-- end
+
+function skip(n,...)
+	return select(n+1,...)
+end
+
+local function skipa(n,...)
+	return {skip(n,...)}
+end
+
+local function _foldr(f,a,...)
+	if select("#",...) == 0 then
+		return a
+	else
+		local x = ...
+		local rest = skipa(1,...)
+		return f(x,function() return _foldr(f,a,unpack(rest)) end)
+	end
+end
+
+function foldr(f,a,xs)
+	return _foldr(f,a,unpack(xs))
+end
+
+function map(f,xs)
+	return foldr(function(x,a) a = a(); table.insert(a,f(x)); return a end,{},xs)
+end
+
+function all(xs)
+	return foldr(function(x,a) return x and a() end,true,xs)
 end
